20e4d8ec6c6cc3add072dbcf83a90144cc07d375
Ivan <ivan.galkin5066@gmail.com>
2021-07-09 20:21:13 +0300

    Divided Live Window on 25 pieces

--- src/GridTest.js
+++ src/GridTest.js
@@ -11,6 +11,81 @@ export default class GridTest extends React.Component {
             <div className='GridContainer'>
                 <div className='LiveWindow'>
                     LiveWindow
+                    <div className='q1'>
+
+                    </div>
+                    <div className='q2'>
+                        
+                    </div>
+                    <div className='q3'>
+                        
+                    </div>
+                    <div className='q4'>
+                        
+                    </div>
+                    <div className='q5'>
+                        
+                    </div>
+                    <div className='q6'>
+                        
+                    </div>
+                    <div className='q7'>
+                        
+                    </div>
+                    <div className='q8'>
+                        
+                    </div>
+                    <div className='q9'>
+                        
+                    </div>
+                    <div className='q10'>
+                        
+                    </div>
+                    <div className='q11'>
+                        
+                    </div>
+                    <div className='q12'>
+                        
+                    </div>
+                    <div className='q13'>
+                        
+                    </div>
+                    <div className='q14'>
+                        
+                    </div>
+                    <div className='q15'>
+                        
+                    </div>
+                    <div className='q16'>
+                        
+                    </div>
+                    <div className='q17'>
+                        
+                    </div>
+                    <div className='q18'>
+                        
+                    </div>
+                    <div className='q19'>
+                        
+                    </div>
+                    <div className='q20'>
+                        
+                    </div>
+                    <div className='q21'>
+                        
+                    </div>
+                    <div className='q22'>
+                        
+                    </div>
+                    <div className='q23'>
+                        
+                    </div>
+                    <div className='q24'>
+                        
+                    </div>
+                    <div className='q25'>
+                        
+                    </div>
                 </div>
                 <div className='ArchiveWindow'>
                     <div className='ContentHolder'>
---
+++ src/LiveDisplay.js
@@ -0,0 +1,11 @@
+import "./ArchiveTable.js";
+
+export default function LiveScreen() {
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
