3f43bedbd4b842ded82ce376d1c62e9553803827
Ivan <ivan.galkin5066@gmail.com>
2021-07-11 12:25:21 +0300

    Добавил отображение времени смены от местного 07:30 (19:30)

--- src/ToolBar.js
+++ src/ToolBar.js
@@ -1,15 +1,46 @@
 import logo from "./assets/images/logo.png";
 import "./ArchiveTable.css";
 import language from "./language.json";
+import React from "react";
+import { StayCurrentLandscapeTwoTone } from "@material-ui/icons";
+
+class Clock extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        time: new Date(),
+      };
+    }
+
+    componentDidMount() {
+      this.intervalID = setInterval(() => this.tick(), 1000);
+    }
+    componentWillUnmount() {
+      clearInterval(this.intervalID);
+    }
+    tick() {
+      this.setState({
+        time: new Date(),
+      });
+    }
+
+    render() {
+        let _time = new Date(new Date() - new Date(Date.UTC(2021, 7, 11, 7, 30, 0)));
+        if(_time.getHours() >= 12) {
+            _time = new Date(new Date() - new Date(Date.UTC(2021, 7, 11, 19, 30, 0)));
+        }
+
+        return <div>{language.ToolBar.TimeGone} {_time.toLocaleTimeString()}</div>
+    }
+}
 
 export default function ToolBarScreen() {
-
 
     return (
         <div className='ToolBarHolder'>
             <img src={logo} className='LogoImage' height="80"/>
             <div className='ToolBarTimeStatus'>
-                {language.ToolBar.TimeGone} 00:00:00
+                <Clock/>
             </div>
             <div className='ToolBarWagonStatus'>
                 {language.ToolBar.WagonsGone} 218
